cf05c7a15146ef068d7df14a1925336da5cb69f1
Eric Savage <79065431+E-Savage@users.noreply.github.com>
2023-10-18 00:03:14 -0400

    Update models.py
    Added blog model to the project

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    """
+        This is a class to mimic a blog post that stores title, content of the blog and also the time
+        that the blgo post was created at
+    """
+
+    title = models.CharField(max_length=200) # tells what control will be used for this data field 
+    content = models.TextField() # shows UI control that will be used for this data field
+    created_at = models.DateTimeField(auto_now_add=True) # uses date field as a UI control, automatically sets the date 
