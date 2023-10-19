942f6f98e7e558473b6320505f7967188733b12e
Eric Savage <eric.n.savage2000@gmail.com>
2023-10-18 23:19:46 -0400

    Registered Post to admin

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register your models here
+# registration is so that it becomes viewable from the admin side of the website
+
+admin.site.register(Post)
