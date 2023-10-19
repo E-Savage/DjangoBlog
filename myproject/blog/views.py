712fc9a641ffcac1df8e175b77f7ebfeaea41d72
Eric Savage <eric.n.savage2000@gmail.com>
2023-10-19 14:08:08 -0400

    updated files

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -10,9 +10,16 @@ from .models import Post
     new view called post_list
 '''
 
+# gets all the posts and displays them
 def post_list(request):
     posts = admin.site.site_header(Post.objects.all())
     return render(request, 'admin/blog/post_list.html', {'posts': posts})
+
+# gets specific post details 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                              public_year=year, publish_month=month, publish_day=day)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 urlpatterns = [
     path('post_list/', post_list),
